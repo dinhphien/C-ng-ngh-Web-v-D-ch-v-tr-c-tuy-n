a4334f56605107ceb735aac58e77118341afdf74
Kinggainer <37549934+Kinggainer@users.noreply.github.com>
2018-05-17 13:57:07 +0700

    Thêm model_tintuc và tintuc

---
+++ models/model_tintuc.php
@@ -0,0 +1,45 @@
+<?php
+require_once("DBconnector.php");
+require_once ("tintuc.php");
+class Model_Tintuc{
+    private $conn;
+
+    /**
+     * Model_Tintuc constructor.
+     * @param $dao
+     */
+    public function __construct(){
+        $dbconnect= new DBConnector();
+        $this->conn = $dbconnect->connect();
+    }
+
+    public function getTT_id($id){
+        $sql="select * from tintuc where tintuc.idtintuc='".$id."';";
+        $result=$this->conn->query($sql);
+        while($row=$result->fetch_assoc()){
+            $tt= new tintuc($row['idtintuc'],$row['tentintuc'],$row['noidungtintuc'],$row['urlanhtintuc'],$row['ngayviet']);
+        }
+        return $tt;
+    }
+
+    public function getTT_ten($ten){
+        $sql="select * from tintuc where tintuc.tentintuc='".$ten."';";
+        $result=$this->conn->query($sql);
+        while($row=$result->fetch_assoc()){
+            $tt= new tintuc($row['idtintuc'],$row['tentintuc'],$row['noidungtintuc'],$row['urlanhtintuc'],$row['ngayviet']);
+        }
+        return $tt;
+    }
+
+    public function getTT(){
+        $sql="select * from tintuc ";
+        $result=$this->conn->query($sql);
+        $array_tt= array();
+        while($row=$result->fetch_assoc()){
+            $tt= new tintuc($row['idtintuc'],$row['tentintuc'],$row['noidungtintuc'],$row['urlanhtintuc'],$row['ngayviet']);
+            array_push($array_tt,$tt);
+        }
+        return $array_tt;
+    }
+}
+?>
