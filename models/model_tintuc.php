7ae2352dd2c892f1568fd07b9af2805c68abd239
Kinggainer <37549934+Kinggainer@users.noreply.github.com>
2018-05-17 22:38:39 +0700

    Thêm sửa xóa tin tức

--- models/model_tintuc.php
+++ models/model_tintuc.php
@@ -47,5 +47,28 @@ class Model_Tintuc{
         $result=$this->conn->query($sql);
         return $result;
     }
+
+    public function insertTT($id,$tentt,$noidung,$urlanhtt,$ngayviet){
+        $sql="insert into tintuc (idtintuc, tentintuc, noidungtintuc, urlanhtintuc, ngayviet) values ('".$id."','".$tentt."','".$noidung."','".$urlanhtt."','".$ngayviet."')";
+        $result=$this->conn->query($sql);
+        return $result;
+    }
+
+    public function updateTT($id,$tentt,$noidung,$url,$ngayviet){
+        $sql="update tintuc set tintuc.tentintuc='".$tentt."', tintuc.noidungtintuc='".$noidung."', tintuc.urlanhtintuc='".$url."', tintuc.ngayviet='".$ngayviet."' where tintuc.idtintuc='".$id."';";
+        $result=$this->conn->query($sql);
+        return $result;
+    }
+
+    public function getTTfilter($id,$tentt){
+        $sql = "select * from tintuc where tintuc.idtintuc like '%" . $id . "%' and tintuc.tentintuc like '%" . $tentt . "%' ";
+        $result = $this->conn->query($sql);
+        $array_tt = array();
+        while ($row = $result->fetch_assoc()) {
+            $tt= new tintuc($row['idtintuc'],$row['tentintuc'],$row['noidungtintuc'],$row['urlanhtintuc'],$row['ngayviet']);
+            array_push($array_tt,$tt);
+        }
+        return $array_tt;
+    }
 }
 ?>
