053f2ee661d9984a994e79c88cecacb81a1eeb4e
Kinggainer <37549934+Kinggainer@users.noreply.github.com>
2018-05-17 13:51:56 +0700

    Sửa blog.php

--- views/khachhang/blog.php
+++ views/khachhang/blog.php
@@ -1,4 +1,7 @@
-<?php include 'header.php' ?>
+<?php
+require_once(__DIR__."/../../models/tintuc.php");
+include 'header.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,9 +19,10 @@
                 <div class="col-sm-12">
                     <ul class="breadcrumb">
 
-                        <li><a href="index.html">Home</a>
+                        <li><a href="index.php">Trang chủ</a>
                         </li>
-                        <li>Blog listing</li>
+                        <li><a href="route.php?controller=controller_tintuc&action=showTT">Bảng tin</a>
+                        </li>
                     </ul>
                 </div>
 
@@ -28,69 +32,34 @@
                 <div class="col-sm-9" id="blog-listing">
 
                     <ul class="breadcrumb">
+                        <h2>Danh sách bài viết</h2>
 
-                        <li><a href="#">Home</a>
-                        </li>
-                        <li>Blog listing</li>
                     </ul>
 
-
-                    <div class="box">
-
-                        <h1>Blog category name</h1>
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.</p>
-
-                    </div>
-
+                    <?php
+                    if(!empty($dataTT)){
+                        foreach($dataTT as $tt){
+                            ?>
                     <div class="post">
-                        <h2><a href="post.html">Fashion now</a></h2>
-                        <p class="author-category">By <a href="#">John Slim</a> in <a href="">Fashion and style</a>
-                        </p>
+                        <h2><a href="route.php?controller=controller_tintuc&action=showTT_detailname&ten_tt=<?php  echo $tt->getTentintuc();?>"><?php  echo $tt->getTentintuc();?></a></h2>
                         <hr>
-                        <p class="date-comments">
-                            <a href="post.html"><i class="fa fa-calendar-o"></i> June 20, 2013</a>
-                            <a href="post.html"><i class="fa fa-comment-o"></i> 8 Comments</a>
+                        
+                        <p class="date">
+                            <h5><i class="fa fa-calendar-o"></i><?php  echo $tt->getNgayviet();?></h5>
                         </p>
+                        
                         <div class="image">
-                            <a href="post.html">
-                                <img src="img/blog2.jpg" class="img-responsive" alt="Example blog post alt">
+                            <a href="route.php?controller=controller_tintuc&action=showTT_detailname&ten_tt=<?php  echo $tt->getTentintuc();?>">
+                                <img src="<?php echo $tt->getUrlanhtintuc(); ?>" alt="" class="img-responsive">
                             </a>
                         </div>
-                        <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
-                            mi vitae est. Mauris placerat eleifend leo.</p>
-                        <p class="read-more"><a href="post.html" class="btn btn-primary">Continue reading</a>
-                        </p>
-                    </div>
 
-
-                    <div class="post">
-                        <h2><a href="post.html">Who is who - example blog post</a></h2>
-                        <p class="author-category">By <a href="#">John Slim</a> in <a href="">About Minimal</a>
-                        </p>
-                        <hr>
-                        <p class="date-comments">
-                            <a href="post.html"><i class="fa fa-calendar-o"></i> June 20, 2013</a>
-                            <a href="post.html"><i class="fa fa-comment-o"></i> 8 Comments</a>
-                        </p>
-                        <div class="image">
-                            <a href="post.html">
-                                <img src="img/blog.jpg" class="img-responsive" alt="Example blog post alt">
-                            </a>
-                        </div>
-                        <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
-                            mi vitae est. Mauris placerat eleifend leo.</p>
-                        <p class="read-more"><a href="post.html" class="btn btn-primary">Continue reading</a>
-                        </p>
-                    </div>
-
-                    <ul class="pager">
-                        <li class="previous"><a href="#">&larr; Older</a>
-                        </li>
-                        <li class="next disabled"><a href="#">Newer &rarr;</a>
-                        </li>
-                    </ul>
-
-
+                    </div>                        
+                    
+                    <?php    
+                        }
+                    }
+                    ?>
 
                 </div>
                 <!-- /.col-md-9 -->
@@ -101,37 +70,13 @@
                 <div class="col-md-3">
                     <!-- *** BLOG MENU ***
  _________________________________________________________ -->
-                    <div class="panel panel-default sidebar-menu">
-
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Blog</h3>
-                        </div>
-
-                        <div class="panel-body">
-
-                            <ul class="nav nav-pills nav-stacked">
-                                <li>
-                                    <a href="blog.html">About us</a>
-                                </li>
-                                <li class="active">
-                                    <a href="blog.html">Fashion</a>
-                                </li>
-                                <li>
-                                    <a href="blog.html">News and gossip</a>
-                                </li>
-                                <li>
-                                    <a href="blog.html">Design</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                    </div>
+ 
                     <!-- /.col-md-3 -->
 
                     <!-- *** BLOG MENU END *** -->
 
                     <div class="banner">
-                        <a href="#">
+                        <a href="route.php?controller=controller_tintuc&action=showTT">
                             <img src="img/banner.jpg" alt="sales 2014" class="img-responsive">
                         </a>
                     </div>
